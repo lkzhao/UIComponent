ac9bf6fc0d97de7811bf18bc944215e1f740ee45
Luke Zhao <me@lkzhao.com>
2018-12-13 21:03:55 -0500

    Initial Commit

---
+++ CollectionKit2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionKit2
+//
+//  Created by Luke Zhao on 2018-12-13.
+//  Copyright © 2018 Luke Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
