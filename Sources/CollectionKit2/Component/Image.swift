746932ce4bb49cbed165e524b15763c06775edcd
Luke Zhao <me@lkzhao.com>
2020-05-02 17:49:54 -0700

    add system name initializer for Image

--- Sources/CollectionKit2/Component/Image.swift
+++ Sources/CollectionKit2/Component/Image.swift
@@ -1,5 +1,5 @@
 //
-//  File 2.swift
+//  Image.swift
 //  
 //
 //  Created by Luke Zhao on 2/5/20.
@@ -11,6 +11,10 @@ public class Image: ViewAdapter<UIImageView> {
   var image: UIImage
   public convenience init(_ imageName: String) {
     self.init(UIImage(named: imageName)!)
+  }
+  @available(iOS 13.0, *)
+  public convenience init(systemName: String) {
+    self.init(UIImage(systemName: systemName) ?? UIImage())
   }
   public init(_ image: UIImage) {
     self.image = image
