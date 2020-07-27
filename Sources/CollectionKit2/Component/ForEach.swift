91ea251a43a79819ebbbbcba7a703b0dde914505
Luke Zhao <me@lkzhao.com>
2020-07-26 20:52:47 -0700

    add join

--- Sources/CollectionKit2/Component/ForEach.swift
+++ Sources/CollectionKit2/Component/ForEach.swift
@@ -13,4 +13,22 @@ public struct ForEach<S: Sequence, D>: ProviderBuilderComponent where S.Element 
   public init(_ data: S, @ProviderBuilder _ content: (D) -> ProviderBuilderComponent) {
     providers = data.flatMap { content($0).providers }
   }
+  
+  private init(providers: [Provider]) {
+    self.providers = providers
+  }
+}
+
+public extension ForEach {
+  func join(@ProviderBuilder _ content: () -> ProviderBuilderComponent) -> Self {
+    var result: [Provider] = []
+    for i in 0..<providers.count - 1 {
+      result.append(providers[i])
+      result.append(contentsOf: content().providers)
+    }
+    if let lastProvider = providers.last {
+      result.append(lastProvider)
+    }
+    return ForEach(providers: result)
+  }
 }
