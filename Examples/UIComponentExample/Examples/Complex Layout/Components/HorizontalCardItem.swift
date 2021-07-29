e811136df9bf4a07e39d5308489d3c32cf8c2e63
hy_iMac <wwdc14@vip.qq.com>
2021-07-29 15:01:01 +0800

    Unified naming

---
+++ Examples/UIComponentExample/Examples/Complex Layout/Components/HorizontalCardItem.swift
@@ -0,0 +1,39 @@
+//
+//  HorizontalCardItem.swift
+//  UIComponentExample
+//
+//  Created by y H on 2021/7/21.
+//  Copyright © 2021 Luke Zhao. All rights reserved.
+//
+
+import UIComponent
+import UIKit.UIScreen
+import UIKit.UITraitCollection
+
+struct HorizontalCardItem: ComponentBuilder {
+  
+  let data: Context
+  
+  func build() -> Component {
+    HStack(spacing: 10, alignItems: .center) {
+      VStack(spacing: 5, justifyContent: .center, alignItems: .center) {
+        Image(systemName: "hand.tap")
+        Text("Tap to delete", font: .systemFont(ofSize: 10))
+      }.inset(10).size(width: .aspectPercentage(1), height: .fill).styleColor(data.fillColor)
+      VStack(justifyContent: .spaceAround) {
+        Text("This is a title").flex()
+        HStack(spacing: 5, alignItems: .center) {
+          Text("This is a description", font: .systemFont(ofSize: 14)).textColor(.secondaryLabel).flex()
+          Image(systemName: "checkmark.shield.fill")
+        }.flex()
+      }.flex()
+    }.inset(10).size(width: 300, height: 100).styleColor(data.fillColor).id(data.id)
+  }
+}
+
+extension HorizontalCardItem {
+  struct Context: Equatable {
+    let id: String = UUID().uuidString
+    let fillColor = UIColor.randomSystemColor()
+  }
+}
---
+++ Examples/UIComponentExample/Examples/Complex Layout/Components/UserProfile.swift
@@ -0,0 +1,54 @@
+//
+//  UserProfile.swift
+//  UIComponentExample
+//
+//  Created by y H on 2021/7/21.
+//  Copyright © 2021 Luke Zhao. All rights reserved.
+//
+
+import UIComponent
+import Kingfisher
+import UIKit.UIImageView
+
+extension ViewComponent {
+  fileprivate func shadowAvatar() -> ViewUpdateComponent<Self> {
+    update {
+      $0.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+      $0.layer.shadowOffset = CGSize(width: 0, height: 3)
+      $0.layer.shadowRadius = 6
+      $0.layer.shadowOpacity = 1
+    }
+  }
+}
+
+struct UserProfile: ComponentBuilder {
+  
+  let avatar: String
+  let userName: String
+  let introduce: String
+  
+  func build() -> Component {
+    HStack(spacing: 10, alignItems: .center) {
+      AsyncImage(avatar)!
+        .contentMode(.scaleAspectFill)
+        .clipsToBounds(true)
+        .size(width: 64, height: 64)
+        .update {
+          $0.layer.cornerRadius = $0.frame.height/2
+        }
+        .with(\.layer.borderWidth, 2)
+        .with(\.layer.borderColor, UIColor.white.cgColor)
+        .view()
+        .id("avatar")
+        .shadowAvatar()
+      
+      VStack(spacing: 4) {
+        Text(userName)
+        Text(introduce, font: .systemFont(ofSize: 13, weight: .light)).textColor(.secondaryLabel)
+        Text("🤔🤔🤔", font: .systemFont(ofSize: 12, weight: .light)).textColor(.secondaryLabel)
+      }.flex()
+      
+      Image(systemName: "chevron.right").tintColor(.systemGray)
+    }.inset(15).defaultShadow().id("user-info")
+  }
+}
