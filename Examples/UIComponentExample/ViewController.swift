2ab237cb9a4c50150a680138bde0ec03ffd34614
wwdc14_home <wwdc14@vip.qq.com>
2021-07-29 19:42:18 +0800

    Remove excess

--- Examples/UIComponentExample/ViewController.swift
+++ Examples/UIComponentExample/ViewController.swift
@@ -20,7 +20,6 @@ class ViewController: ComponentViewController {
         ExampleItem(name: "Flex Layout Example", viewController: FlexLayoutViewController())
         ExampleItem(name: "Waterfall Layout Example", viewController: WaterfallLayoutViewController())
         ExampleItem(name: "Complex Layout Example", viewController: ComplexLayoutViewController())
-        ExampleItem(name: "Irregular Layout Example", viewController: IrregularLayoutViewController())
       } separator: {
         Separator()
       }
