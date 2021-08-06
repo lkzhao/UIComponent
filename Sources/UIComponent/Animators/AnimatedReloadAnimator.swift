5c5bb0a85e928fa2ae18d6634749ff0e9e79c119
Luke Zhao <me@lkzhao.com>
2021-08-05 21:51:48 -0700

    cleanup AnimatedReloadAnimator

--- Sources/UIComponent/Animators/AnimatedReloadAnimator.swift
+++ Sources/UIComponent/Animators/AnimatedReloadAnimator.swift
@@ -9,67 +9,65 @@
 import UIKit
 
 public class AnimatedReloadAnimator: Animator {
-	public static let defaultEntryTransform: CATransform3D = CATransform3DTranslate(CATransform3DScale(CATransform3DIdentity, 0.8, 0.8, 1), 0, 0, -10)
-	public static let fancyEntryTransform: CATransform3D = {
-		var trans = CATransform3DIdentity
-		trans.m34 = -1 / 500
-		return CATransform3DScale(CATransform3DRotate(CATransform3DTranslate(trans, 0, -50, -100), 0.5, 1, 0, 0), 0.8, 0.8, 1)
-	}()
-
-	let entryTransform: CATransform3D
-
-	public init(entryTransform: CATransform3D = defaultEntryTransform) {
-		self.entryTransform = entryTransform
-		super.init()
-	}
-
-	public override func delete(componentView: ComponentDisplayableView, view: UIView) {
-		if componentView.isReloading, componentView.bounds.intersects(view.frame) {
-			UIView.animate(withDuration: 0.25, animations: {
-				view.layer.transform = self.entryTransform
-				view.alpha = 0
-			}, completion: { _ in
-				if !componentView.visibleViews.contains(view) {
-					view.transform = CGAffineTransform.identity
-					view.alpha = 1
-					view.recycleForUIComponentReuse()
-				}
+  let transform: CATransform3D
+  let duration: TimeInterval
+  let cascade: Bool
+  
+  public init(transform: CATransform3D = CATransform3DIdentity, duration: TimeInterval = 0.5, cascade: Bool = false) {
+    self.transform = transform
+    self.duration = duration
+    self.cascade = cascade
+    super.init()
+  }
+  
+  public override func delete(componentView: ComponentDisplayableView, view: UIView) {
+    if componentView.isReloading, componentView.bounds.intersects(view.frame) {
+      UIView.animate(withDuration: duration, animations: {
+        view.layer.transform = self.transform
+        view.alpha = 0
+      }, completion: { _ in
+        if !componentView.visibleViews.contains(view) {
+          view.transform = CGAffineTransform.identity
+          view.alpha = 1
+          view.recycleForUIComponentReuse()
+        }
       })
-		} else {
-			view.recycleForUIComponentReuse()
-		}
-	}
-
-	public override func insert(componentView: ComponentDisplayableView, view: UIView, frame: CGRect) {
-		view.bounds = frame.bounds
-		view.center = frame.center
-		if componentView.isReloading, componentView.hasReloaded, componentView.bounds.intersects(frame) {
-      let offsetTime: TimeInterval = TimeInterval(frame.origin.distance(componentView.bounds.origin) / 3000)
-			view.layer.transform = entryTransform
-			view.alpha = 0
-			UIView.animate(withDuration: 0.5, delay: offsetTime, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
-				view.transform = .identity
-				view.alpha = 1
+    } else {
+      view.recycleForUIComponentReuse()
+    }
+  }
+  
+  public override func insert(componentView: ComponentDisplayableView, view: UIView, frame: CGRect) {
+    view.bounds = frame.bounds
+    view.center = frame.center
+    if componentView.isReloading, componentView.hasReloaded, componentView.bounds.intersects(frame) {
+      let offsetTime: TimeInterval = cascade ? TimeInterval(frame.origin.distance(componentView.bounds.origin) / 3000) : 0
+      view.layer.transform = transform
+      view.alpha = 0
+      UIView.animate(withDuration: duration, delay: offsetTime, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.allowUserInteraction], animations: {
+        view.transform = .identity
+        view.alpha = 1
       })
-		}
-	}
-
-	public override func update(componentView _: ComponentDisplayableView, view: UIView, frame: CGRect) {
-		if view.center != frame.center {
-			UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.layoutSubviews], animations: {
-				view.center = frame.center
+    }
+  }
+  
+  public override func update(componentView _: ComponentDisplayableView, view: UIView, frame: CGRect) {
+    if view.center != frame.center {
+      UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.allowUserInteraction], animations: {
+        view.center = frame.center
       }, completion: nil)
-		}
-		if view.bounds.size != frame.bounds.size {
-			UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.layoutSubviews], animations: {
-				view.bounds.size = frame.bounds.size
+    }
+    if view.bounds.size != frame.bounds.size {
+      UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.allowUserInteraction], animations: {
+        view.bounds.size = frame.bounds.size
       }, completion: nil)
-		}
-		if view.alpha != 1 || view.transform != .identity {
-			UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [], animations: {
-				view.transform = .identity
-				view.alpha = 1
+    }
+    if view.alpha != 1 || view.transform != .identity {
+      UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0, options: [.allowUserInteraction], animations: {
+        view.transform = .identity
+        view.alpha = 1
       }, completion: nil)
-		}
-	}
+    }
+  }
 }
+
