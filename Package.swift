e1aeddede5c979923e5987bb9b4b7a66e84be689
fun <lw_fun@163.com>
2022-07-07 13:00:32 +0800

    123

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/fanglinwei/BaseToolbox", .branchItem("main")),
+    .package(url: "https://github.com/fanglinwei/BaseToolbox", from: "0.1.8"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
