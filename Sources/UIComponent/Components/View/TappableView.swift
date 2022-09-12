34028c549863494eb7d8eb7657c9f0eaa12de090
Luke Zhao <me@lkzhao.com>
2022-09-12 09:52:45 -0700

    passthrough long press gesture

--- Sources/UIComponent/Components/View/TappableView.swift
+++ Sources/UIComponent/Components/View/TappableView.swift
@@ -39,7 +39,7 @@ open class TappableView: ComponentView {
         }
     }
 
-    public var onLongPress: ((TappableView) -> Void)? {
+    public var onLongPress: ((TappableView, UILongPressGestureRecognizer) -> Void)? {
         didSet {
             if onLongPress != nil {
                 addGestureRecognizer(longPressGestureRecognizer)
@@ -149,9 +149,7 @@ open class TappableView: ComponentView {
     }
 
     @objc open func didLongPress() {
-        if longPressGestureRecognizer.state == .began {
-            onLongPress?(self)
-        }
+        onLongPress?(self, longPressGestureRecognizer)
     }
 }
 
