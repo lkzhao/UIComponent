f8c6df49834bbb4e63419698bc3e7bb4a3aed17d
Marcelo Queiroz <marcelorsq@gmail.com>
2023-02-27 18:13:22 -0500

    Fix Image component forced unwrap in init method (#30)
    * Fix Image component forced unwrap in init method
    
    * Add assertions on debug mode
    
    ---------
    
    Co-authored-by: Marcelo Queiroz <marceloqueiroz@pinterest.com>

--- Sources/UIComponent/Components/View/Image.swift
+++ Sources/UIComponent/Components/View/Image.swift
@@ -6,10 +6,33 @@ public struct Image: ViewComponent {
     public let image: UIImage
 
     public init(_ imageName: String) {
-        self.init(UIImage(named: imageName)!)
+#if DEBUG
+        if let image = UIImage(named: imageName) {
+            self.init(image)
+        } else {
+            let error = "Image should be initialized with a valid image name. Image named \(imageName) not found."
+            assertionFailure(error)
+            self.init(UIImage())
+        }
+#else
+        self.init(UIImage(named: imageName) ?? UIImage())
+#endif
+        
     }
 
     public init(systemName: String, withConfiguration configuration: UIImage.Configuration? = nil) {
+#if DEBUG
+        if let systemImage = UIImage(systemName: systemName, withConfiguration: configuration) {
+            self.init(systemImage)
+        } else {
+            let error = "Image should be initialized with a valid system image name. System image named \(systemName) not found."
+            assertionFailure(error)
+            self.init(UIImage())
+        }
+#else
+        self.init(UIImage(systemName: systemName, withConfiguration: configuration) ?? UIImage())
+#endif
+        
         self.init(UIImage(systemName: systemName, withConfiguration: configuration) ?? UIImage())
     }
 
