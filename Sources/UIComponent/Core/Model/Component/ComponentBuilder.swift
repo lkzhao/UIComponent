8d75bc8ceadc995e5983827daf97784346969c91
Luke Zhao <me@lkzhao.com>
2024-01-20 21:09:28 -0700

    add ComponentBuilder doc

--- Sources/UIComponent/Core/Model/Component/ComponentBuilder.swift
+++ Sources/UIComponent/Core/Model/Component/ComponentBuilder.swift
@@ -2,8 +2,13 @@
 
 import Foundation
 
+/// A Component that builds a Component.
+/// Use this instead of ``Component`` when you want to implement ``build()`` instead of ``Component/layout(_:)``.
+/// This saves you from calling ``Component/layout(_:)`` on your child before returning it.
+/// See <doc:CustomComponent> for detail.
 public protocol ComponentBuilder: Component {
     associatedtype ResultComponent: Component
+    /// Builds and returns the `ResultComponent`.
     func build() -> ResultComponent
 }
 
