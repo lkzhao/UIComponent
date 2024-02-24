54a03eedcce4abc7819640ef475bea050fb21bb9
Luke Zhao <me@lkzhao.com>
2024-02-23 17:11:42 -0800

    small fixes (#43)

--- Sources/UIComponent/Components/Join.swift
+++ Sources/UIComponent/Components/Join.swift
@@ -46,9 +46,11 @@ public struct Join: ComponentArrayContainer {
     public init(@ComponentArrayBuilder _ content: () -> [any Component], @ComponentArrayBuilder separator: () -> [any Component]) {
         var result: [any Component] = []
         let components = content()
-        for i in 0..<components.count - 1 {
-            result.append(components[i])
-            result.append(contentsOf: separator())
+        if !components.isEmpty {
+            for i in 0..<components.count - 1 {
+                result.append(components[i])
+                result.append(contentsOf: separator())
+            }
         }
         if let lastComponent = components.last {
             result.append(lastComponent)
