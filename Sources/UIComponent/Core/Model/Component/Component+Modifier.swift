06863bc50b072865ca4db102ed9fec10e5cb3f55
Luke Zhao <me@lkzhao.com>
2025-01-29 17:16:03 -0800

    add view(as:) modifier

--- Sources/UIComponent/Core/Model/Component/Component+Modifier.swift
+++ Sources/UIComponent/Core/Model/Component/Component+Modifier.swift
@@ -243,6 +243,13 @@ extension Component {
         ViewWrapperComponent(component: self)
     }
 
+    /// Wraps the component in a `View`.
+    /// - Parameter viewType: The type of view to use as the wrapper.
+    /// - Returns: A `ViewWrapperComponent` that renders the component within a `View`.
+    public func view<View: UIView>(as viewType: View.Type) -> ViewWrapperComponent<View> {
+        ViewWrapperComponent(component: self)
+    }
+
     /// Wraps the component in a `UIScrollView`.
     /// - Returns: A `ViewWrapperComponent` that renders the component within a `UIScrollView`.
     public func scrollView() -> ViewWrapperComponent<UIScrollView> {
