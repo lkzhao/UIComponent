e41e502567b1297cec2047fffb03b57f3b5ffa89
Luke Zhao <me@lkzhao.com>
2025-04-24 07:37:02 -0700

    add comment

--- Sources/UIComponent/Core/Model/Component/ComponentContextKey.swift
+++ Sources/UIComponent/Core/Model/Component/ComponentContextKey.swift
@@ -1,7 +1,10 @@
 //  Created by Luke Zhao on 4/24/25.
 
 
-
+/// A key for storing and retrieving values in the component context.
+///
+/// Use `.withContext(_ key:value:)` to set the context value for a specific key to a component.
+/// Use `.contextValue(for:)` to retrieve the context value for a specific key from a component.
 public struct ComponentContextKey<Value>: Hashable, Equatable {
     public let rawValue: String
     public init(rawValue: String) {
--- Sources/UIComponent/Core/Model/Component/ComponentContextKeys.swift
+++ Sources/UIComponent/Core/Model/Component/ComponentContextKeys.swift
@@ -2,6 +2,15 @@
 
 import UIKit
 
+/// A keys store for defining the context keys.
+///
+/// To define a new context key, extend this struct and add a new property with a computed property that returns a new instance of `ComponentContextKey`.
+/// For example:
+/// ```swift
+/// extension ComponentContextKeys {
+///     public var myCustomKey: ComponentContextKey<MyCustomType> { .init(rawValue: "myCustomKey") }
+/// }
+/// ```
 public struct ComponentContextKeys {
     public static let shared = ComponentContextKeys()
     public var supportLazyLayout: ComponentContextKey<Bool> { .init(rawValue: "supportLazyLayout") }
