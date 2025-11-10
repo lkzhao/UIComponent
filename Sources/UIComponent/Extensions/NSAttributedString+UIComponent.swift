284c4e7fce70c5cd2f0f187d11b5f5734c7ecaa0
Luke Zhao <me@lkzhao.com>
2025-11-10 14:07:04 -0800

    Fix crash with empty NSAttributedStrin

--- Sources/UIComponent/Extensions/NSAttributedString+UIComponent.swift
+++ Sources/UIComponent/Extensions/NSAttributedString+UIComponent.swift
@@ -9,27 +9,30 @@ extension NSAttributedString {
     // It is the ascender of the first character of an attributed string.
     // It also includes the line height if specified in paragraph style.
     public var ascender: CGFloat {
-        guard let font = self.attribute(.font, at: 0, effectiveRange: nil) as? UIFont else {
+        guard length > 0 else { return 0 }
+
+        guard let font = attribute(.font, at: 0, effectiveRange: nil) as? UIFont else {
             return 0
         }
 
-        guard let paragraphStyle = self.attribute(.paragraphStyle, at: 0, effectiveRange: nil) as? NSParagraphStyle else {
-            return font.ascender;
+        guard let paragraphStyle = attribute(.paragraphStyle, at: 0, effectiveRange: nil) as? NSParagraphStyle else {
+            return font.ascender
         }
 
         var lineHeight = max(font.lineHeight, paragraphStyle.minimumLineHeight)
         if paragraphStyle.maximumLineHeight > 0 {
             lineHeight = min(lineHeight, paragraphStyle.maximumLineHeight)
         }
-        return lineHeight + font.descender;
+        return lineHeight + font.descender
     }
 
     // Returns the descender of an attributed string.
     // It is the descender of the last character's font.
     public var descender: CGFloat {
-        guard let font = self.attribute(.font, at: self.length - 1, effectiveRange: nil) as? UIFont else {
+        guard length > 0 else { return 0 }
+        guard let font = attribute(.font, at: length - 1, effectiveRange: nil) as? UIFont else {
             return 0
         }
-        return font.descender;
+        return font.descender
     }
 }
